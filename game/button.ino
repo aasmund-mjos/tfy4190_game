f124707894d5e88ebb357c99f90404fad3e0848b
p-dahl <anpe@stud.ntnu.no>
2025-02-27 14:52:22 +0100

    Gjort knappen mer egnet for arduino mega

--- game/button.ino
+++ game/button.ino
@@ -8,10 +8,10 @@ void loop()
     int sensorValue1 = analogRead(A4);
     int sensorValue2 = analogRead(A5);
 
-    Serial.print("The X and Y coordinate is:");
-    Serial.print(sensorValue1, DEC);
-    Serial.print(",");
-    Serial.println(sensorValue2, DEC);
-    Serial.println(" ");
-    delay(200);
+    // Serial.print("The X and Y coordinate is:");
+    // Serial.print(sensorValue1, DEC);
+    // Serial.print(",");
+    // Serial.println(sensorValue2, DEC);
+    // Serial.println(" ");
+    // delay(200);
 }
