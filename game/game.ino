ecfdc612d269b975d34d1fe9630318cf5adf4225
aasmund-mjos <asmund.mjos@ntnu.no>
2025-03-01 11:59:14 +0100

    1D - array to 2D - array, plus change to reference grid, so we can easily create different grids for different rooms

--- game/game.ino
+++ game/game.ino
@@ -16,75 +16,73 @@
 #define C   A2
 #define D   A3
 
-int posx = 14;
-int posy = 29;
-int lastposx = 14;
-int lastposy = 29;
+const int grid_size = 32;
+int posx = grid_size/2-2;
+int posy = grid_size-3;
+int lastposx = posx;
+int lastposy = posy;
 
 int jump_sequence = 0;
 
-int grid[32*32] = {
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
-    1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,
-    1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+int coinGrid[grid_size][grid_size] = {0};
+
+const int grid_1[grid_size][grid_size] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+    {1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1},
+    {1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 int num_from_pos(int& x, int& y, int& num)
 {
-  num = x+y*32;
+  num = x+y*grid_size;
 }
 
 void pos_from_num(int& x, int& y, int& num)
 {
-  x = num%32;
-  y = num/32;
+  x = num%grid_size;
+  y = num/grid_size;
 };
 
-void draw_matrix()
+void draw_matrix(const int (&grid)[grid_size][grid_size])
 {
   matrix.fillScreen(matrix.Color333(0,0,0));
-  int x = 0;
-  int y = 0;
-  for (int i = 0; i < 32*32; ++i)
+  for (int x = 0; x < grid_size; ++x)
   {
-    int current = grid[i];
-
-    if (current==1)
-      {
-        pos_from_num(x,y,i);
-        matrix.drawPixel(x,y,matrix.Color333(7,7,7));
-      }
+    for (int y = 0; y < grid_size; ++y)
+    {
+      if (grid[y][x])    {matrix.drawPixel(x,y,matrix.Color333(7,7,7));}
+    }
   }
 }
 
@@ -94,29 +92,17 @@ void draw_player()
   matrix.fillRect(posx,posy,2,2, matrix.Color333(0,7,0));
 }
 
-bool object_in_way(int x,int y)
+bool object_in_way(int x, int y, const int (&grid)[grid_size][grid_size])
 {
-  int num = 0;
-
-  num_from_pos(x,y,num);
-  if (grid[num])  {return 1;}
-
-  int new_x = x+1;
-  num_from_pos(new_x,y,num);
-  if (grid[num])  {return 1;}
-
-  int new_y = y+1;
-  num_from_pos(x,new_y,num);
-  if (grid[num])  {return 1;}
-
-  num_from_pos(new_x,new_y,num);
-  if (grid[num])  {return 1;}
+  if (grid[y][x])     {return 1;}
+  if (grid[y][x+1])   {return 1;}
+  if (grid[y+1][x])   {return 1;}
+  if (grid[y+1][x+1]) {return 1;}
 
   return 0;
-
 }
 
-void get_input()
+void get_input(const int (&grid)[grid_size][grid_size])
 {
   lastposx = posx;
   lastposy = posy;
@@ -127,19 +113,14 @@ void get_input()
   {
     if (jump_sequence == 1 or jump_sequence == 3)
     {
-      if (posy>1 and !object_in_way(posx,posy-1))     {posy-=1;}
-      else                                            {jump_sequence = 4;}  // should go to 5, but since we increment jump sequence, we say 4
+      if (!object_in_way(posx,posy-1, grid))                {posy-=1;}
+      else                                                  {jump_sequence = 4;}  // should go to 5, but since we increment jump sequence, we say 4
     }
-
-    // if (jump_sequence == 2 or jump_sequence == 4 or jump_sequence == 5 or jump_sequence == 6 or jump_sequence == 8)
-    // {
-    //   // do nothing, so can probably remove this  
-    // }
 
     if (jump_sequence == 7 or jump_sequence == 9 or jump_sequence == 10)
     {
-      if (!object_in_way(posx, posy+1))   {posy+=1;}
-      else                                {jump_sequence = 10;}
+      if (!object_in_way(posx, posy+1, grid))   {posy+=1;}
+      else                                      {jump_sequence = 10;}     // finalize jump sequence
     }
 
     jump_sequence+=1;
@@ -150,69 +131,68 @@ void get_input()
   else //if not in a jump, then try to move player down, and move left/right if input for that, or check if we should initiate jump sequence
 
   {
-    if (!object_in_way(posx, posy+1))   {posy+=1;}
+    if (!object_in_way(posx, posy+1, grid))   {posy+=1;}
 
     else
     {
       if (y_val > 550)  // initiate jump
       {
-        if (posy > 1 and !object_in_way(posx,posy-1))     {posy-=1;   jump_sequence=1;}
+        if (!object_in_way(posx,posy-1, grid))     {posy-=1;   jump_sequence=1;}  // initiate jump
       }
     }
   }
 
   if (x_val < 490)  // move left
   {
-    if (posx > 1 and !object_in_way(posx-1,posy))   {posx-=1;}
+    if (!object_in_way(posx-1,posy, grid))   {posx-=1;}
   }
 
   if (x_val > 550)  // move right
   {
-    if (posx < 29 and !object_in_way(posx+1,posy))    {posx+=1;}
+    if (!object_in_way(posx+1,posy, grid))    {posx+=1;}
   }
   delay(100);
 }
 
 void draw_border()
 {
-  for (int i = 0; i < 32; ++i)
+  for (int i = 0; i < grid_size; ++i)
   {
     matrix.drawPixel(i,0,matrix.Color333(7,0,0));
-    matrix.drawPixel(i,31,matrix.Color333(7,0,0));
+    matrix.drawPixel(i,grid_size-1,matrix.Color333(7,0,0));
   }
 
-  for (int i = 0; i < 31; ++i)
+  for (int i = 0; i < grid_size-1; ++i)
   {
     matrix.drawPixel(0,i,matrix.Color333(7,0,0));
-    matrix.drawPixel(31,i,matrix.Color333(7,0,0));
+    matrix.drawPixel(grid_size-1,i,matrix.Color333(7,0,0));
   }
 }
 
-void generate_coins()
+void generate_coins(const int (&grid)[grid_size][grid_size])
 {
-  const int grid_size = sizeof(grid) / sizeof(grid[0]);
-  int coinGrid[grid_size] = {0}; 
+  int randomNumber;
+  int x;
+  int y;
 
   for (int i = 0; i < 20; ++i) //Satte 20 som antall coins, men dette kan jo endres
   {
-    int randomNumber = random(grid_size);
+    int randomNumber = random(grid_size*grid_size);
+    pos_from_num(randomNumber, x, y); 
 
-    if (grid[randomNumber] != 1 && coinGrid[randomNumber] == 0)
-    {
-      int x, y;
-      pos_from_num(randomNumber, x, y); 
+    if (grid[y][x] != 1 && coinGrid[y][x] == 0)
+    { 
       matrix.drawPixel(x, y, matrix.Color333(0, 7, 7));
-
-      coinGrid[randomNumber] = 1; // Markerer at en mynt er plassert her
+      coinGrid[y][x] = 1; // Markerer at en mynt er plassert her
     }
-    else {
+    else
+    {
       --i; // Prøver igjen
     }
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
 
   matrix.begin();
   matrix.setCursor(1, 1);    // start at top left, with one pixel of spacing
@@ -225,13 +205,12 @@ void setup() {
   matrix.println("Climb");
   draw_border();
   delay(5000);
-  draw_matrix();
+  draw_matrix(grid_1);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  get_input();
-  // draw_matrix();
+
+  get_input(grid_1);            // with the condition to "open" the door to the next level we can have checks and an integer to keep track of what grid we use.
   draw_player();
 }
